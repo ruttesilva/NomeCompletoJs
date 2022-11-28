0b19d99b075060c58704a58d38fa785359774a60
Rutte <rosrosrutte@gmail.com>
2022-11-27 22:48:39 -0300

    Finaliza exercício

---
+++ index.js
@@ -0,0 +1,7 @@
+let nome = prompt('Digite seu nome: ')
+
+let sobrenome = prompt('Digite seu sobrenome: ')
+
+document.write ('Nome completo: ' + nome + ' ' + sobrenome)
+document.write("<br>");
+document.write ('Nome de catálago: ' + sobrenome.toUpperCase() + ', ' + ' ' + nome) 
